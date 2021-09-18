4a5bc06d17da6780efc8561db7b3b303e72de725
Farhan <74499472+fb0801@users.noreply.github.com>
2021-09-18 18:27:33 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -6,18 +6,24 @@ function moveLeft(){
     //moving the ball left
     var left =
     parseInt(window.getComputedStyle(character).getPropertyValue("left"));
-    character.style.left = left + 2 + "px";
+    if(left > 0){
+        //prevent user going out of game range
+        character.style.left = left - 2 + "px";
+} 
 }
 
 function moveRight(){
     //moving the ball left
     var left =
     parseInt(window.getComputedStyle(character).getPropertyValue("left"));
-    character.style.left = left - 2 + "px";
+    if(left <380){
+    character.style.left = left + 2 + "px";
+}
 }
 
 document.addEventListener("keydown", event => {
     if (both ==0){
+        both++;
     //make the ball move when user presses the key
     if (event.key==="ArrowLeft"){
         interval = setInterval(moveLeft,1)
@@ -30,6 +36,14 @@ document.addEventListener("keydown", event => {
 document.addEventListener("keyup", event => {
     //stop listening when the user no longer holding the key
     clearInterval(interval);
+    both=0;
 
 
 });
+
+
+//elements for the game
+var block = document.createElement('div');
+var hole = document.createElement('div');
+block.setAttribute("class", "block");
+block.setAttribute("class", "block");
